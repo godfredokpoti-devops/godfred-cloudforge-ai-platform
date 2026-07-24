1840a25578a02f2c50ffb764ebdf73033030c553
Godfred Okpoti <godfred.os.deyoung@gmail.com>
2026-07-23 21:48:09 -0400

    Phase 1: Bootstap infrastructure with Terraform remote state

---
+++ terraform/bootstrap/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for backend resources."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "state_bucket_name" {
+  description = "Globally unique S3 bucket name for Terraform state."
+  type        = string
+  default     = "godfred-cloudforge-ai-platform-tfstate"
+}
+
+variable "lock_table_name" {
+  description = "DynamoDB table name for Terraform state locking."
+  type        = string
+  default     = "cloudforge-ai-platform-tflock"
+}
